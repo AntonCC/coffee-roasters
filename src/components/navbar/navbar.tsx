65c75669965f9fe6dbc8a8cb442b14036faf25a4
Anton <antonchetvv@gmail.com>
2021-02-13 23:27:43 -0500

    Home page looking good on all viewport.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 import './navbar.scss'
 import { NavLink } from 'react-router-dom'
 import Logo from '../../assets/shared/desktop/logo.svg'
+import LogoMobile from '../../assets/shared/mobile/logo-mobile.svg'
+import { ReactComponent as Hamburger } from '../../assets/shared/mobile/icon-hamburger.svg'
 
 const Navbar: React.FC = () => {
   return (
     <div className='navbar container'>
+        {/* <picture>
+            <source media="(min-width:650px)" srcset="img_pink_flowers.jpg">
+            <source media="(min-width:465px)" srcset="img_white_flower.jpg">
+            <img src="img_orange_flowers.jpg" alt="Flowers" style="width:auto;">
+          </picture> */}
         <div className="logo">
-          <img src={Logo} alt="Brand logo"/>
+          <picture>
+            <source media="(max-width: 530px)" srcSet={LogoMobile} />
+            <img src={Logo} alt="Logo"/>
+          </picture>
         </div>
         <ul>
           <li>
@@ -16,6 +26,9 @@ const Navbar: React.FC = () => {
             <NavLink to='/create-plan'>Create Your Plan</NavLink>
           </li>
         </ul>
+        <div className="hamburger-menu">
+          <Hamburger />
+        </div>
     </div>
   )
 }
